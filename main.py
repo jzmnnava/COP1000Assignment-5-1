57962c8f7619f953bf966f8a3f8f2558a64db9e3
jzmnnava <146144314+jzmnnava@users.noreply.github.com>
2024-09-13 22:10:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+#Initialize variables 
+a = 1 
+b = 2
+c = 5 
+
+#While Loop: Continue until a is less than c 
+while a < c :
+  a += 1 
+  b += c 
+#Output 
+print (a, b, c)
+
+#Initialize variables
+d = 4
+e = 6
+f = 7 
+
+#While Loop: Continue until d is greater than f 
+while d > f :
+  d += 1 
+  e -= 1 
+#Output 
+print (d, e, f)
+
+#Initialize variables
+g = 4
+h = 6
+
+#While Loop: Continue until g is less than h 
+while g < h :
+  g += 1
+#Output 
+print (g, h)
+
+#Initialize variables
+j = 2
+k = 5
+n = 9 
+
+#While Loop: Continue until j is less than k 
+while j < k :
+  m = 6
+  while m < n :
+      print ("Goodbye")
+      m += 1
+  j += 1
+
+#Initialize variables
+j = 2
+k = 5
+m = 6
+n = 9
+
+#While Loop: Continue until j is less than k 
+while j < k :
+    while m < n : 
+      print ("Hello")
+      m += 1
+    j +=1
+
+#Initialize variables
+p = 2
+q = 4
+
+#While Loop: Continue until p is less than q
+while p < q : 
+  print ("Adios")
+  r = 1
+  while r < q :
+    print ("Adios")
+    r += 1
+  p += 1  
